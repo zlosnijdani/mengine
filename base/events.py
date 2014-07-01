65b00d85922f417850b60f49919cc5a37f767b39
Aleksey Brek <zlosnijdani@gmail.com>
2014-07-01 17:07:46 +0300

    Base events

--- base/events.py
+++ base/events.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python                                                                                                                                                                      
 #-*- coding: utf-8 -*-
 
-import schematics
+from schematics.models import Model
+from schematics.types import base
+
+
+class BaseEvent(Model):
+
+    type = None
+    signature = base.StringType(default=None)
+
+
+class Authorize(BaseEvent):
+
+    type = 'authorize'
+
+    user_id = base.StringType()
+    network = base.StringType()
